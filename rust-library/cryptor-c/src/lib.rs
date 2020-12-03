4fc59b79f3ad3b8f857ec059d46e0aa7786d304b
Fernando Cejas <fcejas@gmail.com>
2020-12-03 17:35:43 +0100

    Add base64 encoding to Rust Library.

--- rust-library/cryptor-c/src/lib.rs
+++ rust-library/cryptor-c/src/lib.rs
@@ -1,10 +1,12 @@
 use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 
-#[no_mangle]
+/**
+ * Encrypt a String.
+ */
+#[no_mangle] 
 pub extern "C" fn encrypt(to: *const c_char) -> *mut c_char {
     let to = unsafe { CStr::from_ptr(to).to_str().unwrap() };
-
     let result = cryptor::encrypt(to);
 
     CString::new(result).unwrap().into_raw()
@@ -16,9 +18,9 @@ mod tests {
 
     #[test]
     fn test_encrypt_string() {
-        let recipient = "hello";
-        let recipient_cstring = CString::new(recipient).unwrap();
-        let result = encrypt(recipient_cstring.as_ptr());
+        let to_encrypt = "hello";
+        let to_encrypt_cstring = CString::new(to_encrypt).unwrap();
+        let result = encrypt(to_encrypt_cstring.as_ptr());
         let result = unsafe { CStr::from_ptr(result).to_string_lossy().into_owned() };
 
         assert_eq!("hello:sdflsjdfsv094", result);
--- rust-library/cryptor/src/lib.rs
+++ rust-library/cryptor/src/lib.rs
@@ -1,5 +1,25 @@
+extern crate base64;
+
+/**
+ * Encrypt a String.
+ */
 pub fn encrypt(to: &str) -> String {
-    to.to_owned() + ":sdflsjdfsv094"
+    let b64 = base64::encode(to);
+    
+    b64.to_owned()
+}
+
+/**
+ * Decrypt a String.
+ */
+pub fn decrypt(from: &str) -> String {
+    let bytes = base64::decode(from).unwrap();
+    let str_result = match String::from_utf8(bytes) {
+        Ok(value) => value,
+        Err(error) => panic!("Invalid UTF-8 sequence: {}", error),
+    };
+
+    str_result.to_owned()
 }
 
 #[cfg(test)]
@@ -8,8 +28,21 @@ mod tests {
 
     #[test]
     fn test_encrypt_string() {
-        let recipient = "hello";
-        let result = encrypt(&recipient);
-        assert_eq!("hello:sdflsjdfsv094", result);
+        let to_encrypt = "hello_world";
+        let encrypted = "aGVsbG9fd29ybGQ=";
+
+        let result = encrypt(&to_encrypt);
+        
+        assert_eq!(encrypted, result);
+    }
+
+    #[test]
+    fn test_decrypt_string() {
+        let to_decrypt = "aGVsbG9fd29ybGQ=";
+        let decrypted = "hello_world";
+
+        let result = decrypt(&to_decrypt);
+        
+        assert_eq!(decrypted, result);
     }
 }
