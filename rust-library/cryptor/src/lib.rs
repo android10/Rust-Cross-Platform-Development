a4972ecc6deec4d2c606a2ba81c6a658b2ac173f
Franziskus Kiefer <franziskuskiefer@gmail.com>
2020-12-03 14:33:01 +0100

    reorg

--- rust-library/cryptor/src/lib.rs
+++ rust-library/cryptor/src/lib.rs
@@ -1,45 +1,15 @@
-use std::os::raw::{c_char};
-use std::ffi::{CString, CStr};
-
-#[no_mangle]
-pub extern fn encrypt(to: *const c_char) -> *mut c_char {
-    let c_str = unsafe { CStr::from_ptr(to) };
-    let recipient = match c_str.to_str() {
-        Err(_) => "There was an error",
-        Ok(string) => string,
-    };
-
-    CString::new("sdflsjdfsv094".to_owned()).unwrap().into_raw()
+pub fn encrypt(to: &str) -> String {
+    to.to_owned() + ":sdflsjdfsv094"
 }
 
-/// Expose the JNI interface for Android
-#[cfg(target_os="android")]
-#[allow(non_snake_case)]
-pub mod android {
-    extern crate jni;
-
+#[cfg(test)]
+mod tests {
     use super::*;
-    use self::jni::JNIEnv;
-    use self::jni::objects::{JClass, JString};
-    use self::jni::sys::{jstring};
 
-    #[no_mangle]
-    pub unsafe extern fn Java_com_fernandocejas_cryptor_encrypt(env: JNIEnv, _: JClass, java_pattern: JString) -> jstring {
-        let string = encrypt(env.get_string(java_pattern).expect("invalid pattern string").as_ptr());
-        // Retake pointer so that we can use it below and allow memory to be freed when it goes out of scope.
-        let string_ptr = CString::from_raw(string);
-        let output = env.new_string(string_ptr.to_str().unwrap()).expect("Couldn't create java string!");
-
-        output.into_inner()
+    #[test]
+    fn test_encrypt_string() {
+        let recipient = "hello";
+        let result = encrypt(&recipient);
+        assert_eq!("hello:sdflsjdfsv094", result);
     }
 }
-
-// #[cfg(test)]
-// mod tests {
-//     use super::encrypt;
-
-//     #[test]
-//     fn test_encrypt_string() {
-//         assert_eq!("sdflsjdfsv094", encrypt("hello".as_ptr() as *const i8));
-//     }
-// }
