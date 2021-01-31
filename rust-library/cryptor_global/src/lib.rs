edd52a3424d856f6df1b51620bfd69767342f385
Fernando Cejas <fcejas@gmail.com>
2021-01-31 21:54:40 +0100

    Working on Unit Tests: WIP.

--- rust-library/cryptor_global/src/lib.rs
+++ rust-library/cryptor_global/src/lib.rs
@@ -5,6 +5,7 @@ pub mod io {
     use std::fs;
     use std::path::PathBuf;
     use std::fs::File;
+    use std::io::Result;
 
     pub fn create_cargo_config_file(dir_path: &PathBuf) -> File {
         let config_dir = dir_path.join(super::CARGO_CONFIG_DIR_NAME);
@@ -15,12 +16,17 @@ pub mod io {
             Ok(_) => println!("Successfully created Cargo Configuration Directory."),
         }
 
-        File::create(&config_file).expect("Could not create cargo configuration file")
+        create_config_file(&config_file).expect("Could not create cargo configuration file")
+    }
+
+    fn create_config_file(file_path: &PathBuf) -> Result<File> {
+        File::create(&file_path)
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use super::io::*;
 
     mod io_tests {
         #[test]
