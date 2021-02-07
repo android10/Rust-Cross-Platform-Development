6641db27d603541bfe78533e826268743f897917
Fernando Cejas <fcejas@gmail.com>
2021-02-07 22:45:01 +0100

    Add build commands when release rust jni library.

--- rust-library/cryptor_jni/release.rs
+++ rust-library/cryptor_jni/release.rs
@@ -3,13 +3,32 @@ use cryptor_global::console;
 
 pub fn build_android_artifacts() {
     if system::is_release() {
-        //TODO
-        // console::run_command(comand: &str, args: &[&str])
+        //TODO: Please refactor this
+        let command_base = "cargo";
+        let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
+        console::run_command(&command_base, &command_args);
+        let command_args = ["build", "--target", "aarch64-linux-android", "--release"];
+        console::run_command(&command_base, &command_args);
+        let command_args = ["build", "--target", "i686-linux-android", "--release"];
+        console::run_command(&command_base, &command_args);
+        let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
+        console::run_command(&command_base, &command_args);
     }
 }
 
 pub fn deploy_android_artifacts() {
     if system::is_release() {
         //TODO: copy the artifacts to its corresponding directories
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //TODO: Write tests
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
     }
 }
