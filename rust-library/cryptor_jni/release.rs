5c54d5fd41dbaea94cfa27fcb7ec106dcccef807
Fernando Cejas <fcejas@gmail.com>
2021-02-08 23:09:37 +0100

    Comment/Fix code to exit endless loop.

--- rust-library/cryptor_jni/release.rs
+++ rust-library/cryptor_jni/release.rs
@@ -4,15 +4,15 @@ use cryptor_global::console;
 pub fn build_android_artifacts() {
     if system::is_release() {
         //TODO: Please refactor this
-        let command_base = "cargo";
-        let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
-        console::run_command(&command_base, &command_args);
-        let command_args = ["build", "--target", "aarch64-linux-android", "--release"];
-        console::run_command(&command_base, &command_args);
-        let command_args = ["build", "--target", "i686-linux-android", "--release"];
-        console::run_command(&command_base, &command_args);
-        let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
-        console::run_command(&command_base, &command_args);
+        // let command_base = "cargo";
+        // let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
+        // console::run_command(&command_base, &command_args);
+        // let command_args = ["build", "--target", "aarch64-linux-android", "--release"];
+        // console::run_command(&command_base, &command_args);
+        // let command_args = ["build", "--target", "i686-linux-android", "--release"];
+        // console::run_command(&command_base, &command_args);
+        // let command_args = ["build", "--target", "armv7-linux-androideabi", "--release"];
+        // console::run_command(&command_base, &command_args);
     }
 }
 
