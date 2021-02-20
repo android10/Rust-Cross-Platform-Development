332d3f8511ac5a3a738003207770966d7de503a6
Fernando Cejas <fcejas@gmail.com>
2021-02-20 12:04:27 +0100

    Remove useles release.rs causing an endless loop.

--- rust-library/cryptor_jni/build.rs
+++ rust-library/cryptor_jni/build.rs
@@ -5,14 +5,10 @@
 use cryptor_global::system;
 
 mod config;
-mod release;
 
 fn main() {
     system::rerun_if_changed("build.rs");
-
+    
     config::create_android_targets_config_file();
     config::add_android_targets_to_toolchain();
-    
-    release::build_android_artifacts();
-    release::deploy_android_artifacts();
 }
