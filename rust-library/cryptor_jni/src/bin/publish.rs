e1a203ea6d957cd43244de36b4712e93d22ef6fb
Fernando Cejas <fcejas@gmail.com>
2021-05-13 22:57:15 +0200

    Add Android ABI map in order to publish rust generated android targets.

--- rust-library/cryptor_jni/src/bin/publish.rs
+++ rust-library/cryptor_jni/src/bin/publish.rs
@@ -2,6 +2,40 @@
 
 #[path="../../build.rs"] mod build;
 
+use phf::phf_map;
+
+/**
+ * This map is necessary for copying each generated target `libcryptor_jni.so` and `libcryptor.so` to 
+ * their specific directory inside the `jniLibs` in the android project.  
+ * 
+ * Each generated Target relates to an ABI (Application Binary Interface, which is a combination of 
+ * CPU type and instruction set). According to the official android documentation, we map each
+ * target with its corresponding directory (ABI): 
+ *
+ * -------------------------------------------
+ *  ABI               TARGET
+ *  ------------------------------------------
+ *  armeabi-v7a	 ---> armv7a-linux-androideabi
+ *  arm64-v8a    ---> aarch64-linux-android
+ *  x86	         ---> i686-linux-android
+ *  x86-64       ---> x86_64-linux-android
+ * -------------------------------------------
+ * 
+ * For more information, check the Official Android documentation: 
+ *  - https://developer.android.com/ndk/guides/other_build_systems
+ *  - https://developer.android.com/ndk/guides/abis
+ */
+static ABI_TARGETS: phf::Map<&'static str, &'static str> = phf_map! {
+    "armeabi-v7a" => "armv7a-linux-androideabi",
+    "arm64-v8a" => "aarch64-linux-android",
+    "x86" => "i686-linux-android",
+    "x86-64" => "x86_64-linux-android",
+};
+
+static LIB_CRYPTOR_JNI_FILENAME: &str = "libcryptor_jni.so";
+static LIB_CRYPTOR_FILENAME: &str = "libcryptor.so";
+
+
 fn main() {
     println!("PUBLISH: {}", &build::ANDROID_TARGETS.len());
     println!("rust-library/target/armv7-linux-androideabi/release/libcryptor_jni.so");
